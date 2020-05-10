800caccb023ad8ef8b7f83557e4706c79b301689
infinityCounter <emilekeith3.0@gmail.com>
2020-05-10 02:31:16 -0500

    Fix the default value of a public trade.

--- lib/src/common/public_trade.dart
+++ lib/src/common/public_trade.dart
@@ -4,8 +4,8 @@ class PublicTrade {
   int id = 0;
   int timestamp = 0;
 
-  num price = 1;
-  num amount = 2;
+  num price = 0;
+  num amount = 0;
 
   toString() {
     return "PublicTrade(id=${this.id},  timestamp=${this.timestamp}, price=${this.price}, amount=${this.amount})";
