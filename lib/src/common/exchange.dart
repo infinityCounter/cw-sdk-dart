34246d8e393a229f1304d5ae9227717436cf0020
infinityCounter <emilekeith3.0@gmail.com>
2020-05-10 02:26:17 -0500

    Remove unnecessary constructures. By having the fields be required to instantiate the new objects, it was making the classes less testable and less comfortable/flexible.

--- lib/src/common/exchange.dart
+++ lib/src/common/exchange.dart
@@ -8,8 +8,6 @@ class Exchange {
 
   bool active;
 
-  Exchange(this.id, this.name, this.symbol, this.active);
-
   toString() {
     return "Exchange(id=${this.id}, name=${this.name}, symbol=${this.symbol}, active=${this.active})";
   }
--- lib/src/common/market.dart
+++ lib/src/common/market.dart
@@ -8,8 +8,6 @@ class Market {
 
   bool active;
 
-  Market(this.id, this.exchange, this.pair, this.active);
-
   toString() {
     return "Market(id=${this.id}, exchange=${this.exchange}, pair=${this.pair}, active=${this.active})";
   }
