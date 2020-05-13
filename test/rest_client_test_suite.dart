11921a4c5394282bf15cb759b5f44f020faf4cb5
infinityCounter <emilekeith3.0@gmail.com>
2020-05-12 21:24:13 -0500

    Got first working test. Create an object that operates similarly to go tests where all private, static methods prefixed with test_ are executed by default. The only example so far initializes a list of test cases and then passes them off to a runner function which instantiates the API client and makes the request.
    Pretty niffty I think.

---
+++ test/rest_client_test_suite.dart
@@ -0,0 +1,180 @@
+part of cw_sdk_dart_test;
+
+const _testApiDomain = "test-api.cryptowat.ch";
+const _testApiKey = "827e9876-2e28-403b-b277-9f17d892337c";
+
+class restApiClientTestCase {
+  String descr;
+
+  String setDomain;
+  String setApiKey;
+
+  List posArgs;
+  Map<String, dynamic> namedArgs;
+
+  String wantUrl;
+  String methodName;
+
+  int respStatusCode;
+  String respJson;
+
+  var wantRes;
+  var wantException;
+}
+
+class restApiClientTestSet {
+  String testGroupName;
+
+  List<restApiClientTestCase> cases = [];
+}
+
+class restApiClientTestSuite {
+  static runTests() {
+    var testFuncIMs = _getAllTestFuncInstanceMirrors();
+
+    for (var tf in testFuncIMs) {
+      tf.reflectee();
+    }
+  }
+
+  static void _test_f1() {
+    var btc = sdk.Asset()
+      ..id = 60
+      ..symbol = "btc"
+      ..name = "Bitcoin"
+      ..fiat = false;
+
+    var testSet = restApiClientTestSet()
+      ..testGroupName = "_test_f1"
+      ..cases = [
+        restApiClientTestCase()
+          ..descr = "Fetching Bitcoin from API"
+          ..methodName = "fetchAsset"
+          ..posArgs = ["btc"]
+          ..setDomain = _testApiDomain
+          ..wantUrl = "https://${_testApiDomain}/assets/btc"
+          ..respJson = '''
+          {
+            "result": {
+              "id":60,
+              "symbol":"btc",
+              "name":"Bitcoin",
+              "fiat":false,
+              "markets":{
+                "base":[
+                  {
+                    "id":4635,
+                    "exchange":"kraken-futures",
+                    "pair":"btcusd-quarterly-futures",
+                    "active":true,
+                    "route":"https://api.cryptowat.ch/markets/kraken-futures/btcusd-quarterly-futures"
+                  }
+                ],
+                "quote":[
+                  {
+                    "id":60917,
+                    "exchange":"kraken-futures",
+                    "pair":"xrpbtc-perpetual-futures",
+                    "active":true,
+                    "route":"https://api.cryptowat.ch/markets/kraken-futures/xrpbtc-perpetual-futures"
+                    }
+                  ]
+                }
+              },
+            "allowance":{
+              "cost":1117794,
+              "remaining": 3998882206,
+              "remainingPaid":0,
+              "upgrade": "Upgrade for a higher allowance, starting at \$15/month for 16 seconds/hour. https://cryptowat.ch/pricing"
+            }
+          }
+          '''
+          ..respStatusCode = 200
+          ..wantRes = btc
+      ];
+
+    return _runRestApiClientTestSet(testSet);
+  }
+
+  static List<mirrors.InstanceMirror> _getAllTestFuncInstanceMirrors() {
+    var testFuncIMs = List<mirrors.InstanceMirror>();
+
+    var thisClass = mirrors.reflectClass(restApiClientTestSuite);
+    var voidMir = mirrors.currentMirrorSystem().voidType;
+
+    thisClass.staticMembers.forEach((k, methodMir) {
+      if (methodMir.isGetter ||
+          methodMir.isSetter ||
+          !methodMir.isPrivate ||
+          !methodMir.isStatic ||
+          methodMir.returnType != voidMir) {
+        return;
+      }
+
+      String methodName = mirrors.MirrorSystem.getName(methodMir.simpleName);
+      if (!methodName.startsWith("_test_")) {
+        return;
+      }
+
+      var testFuncIM = thisClass.getField(methodMir.simpleName);
+
+      testFuncIMs.add(testFuncIM);
+    });
+
+    return testFuncIMs;
+  }
+
+  static _runRestApiClientTestSet(restApiClientTestSet testSet) {
+    if (testSet.cases.length == 0) {
+      return;
+    }
+
+    testing.group(testSet.testGroupName, () {
+      for (var tc in testSet.cases) {
+        var clientMirror;
+        var methodSym = mirrors.MirrorSystem.getSymbol(tc.methodName);
+
+        {
+          var mockHttpClient = httpTesting.MockClient((http.Request req) {
+            testing.expect(req.url.toString(), testing.equals(tc.wantUrl));
+
+            if (req.url.toString() != tc.wantUrl) {
+              return Future.value(http.Response("Not Found", 404));
+            }
+
+            return Future.value(http.Response(tc.respJson, tc.respStatusCode));
+          });
+
+          var apiClient = sdk.RestApiClient(
+            httpClient: mockHttpClient,
+            apiDomain: tc.setDomain,
+            apiKey: tc.setApiKey,
+          );
+
+          clientMirror = mirrors.reflect(apiClient);
+        }
+
+        testing.test(tc.descr, () async {
+          Map<Symbol, dynamic> namedArgs;
+
+          if (tc.namedArgs != null) {
+            tc.namedArgs.map((k, v) {
+              var kSym = mirrors.MirrorSystem.getSymbol(k);
+              return MapEntry(kSym, v);
+            });
+          }
+
+          var resMir = clientMirror.invoke(methodSym, tc.posArgs, namedArgs);
+          var gotRes = resMir.reflectee;
+
+          if (gotRes is Future) {
+            Future resFuture = gotRes;
+            gotRes = await resFuture;
+          }
+
+          testing.expect(gotRes, testing.equals(tc.wantRes));
+        });
+      }
+    });
+  }
+}
--- test/rest_test.dart
+++ test/rest_test.dart
@@ -1,6 +1,8 @@
 library cw_sdk_dart_test;
 
 import 'dart:convert' as convert;
+import 'dart:ffi';
+import 'dart:io';
 import 'dart:mirrors' as mirrors;
 
 import 'package:test/test.dart' as testing;
@@ -9,102 +11,8 @@ import 'package:http/testing.dart' as httpTesting;
 
 import 'package:cw_sdk_dart/cw_sdk_dart.dart' as sdk;
 
-part 'test_case.dart';
+part 'rest_client_test_suite.dart';
 
 main() {
-  const apiDomain = "test-api.cryptowat.ch";
-  const apiKey = "827e9876-2e28-403b-b277-9f17d892337c";
-
-  var anonApiClient = sdk.RestApiClient();
-  var altDomainApiClient = sdk.RestApiClient(apiDomain: apiDomain);
-  var authenticatedApiClient = sdk.RestApiClient(apiKey: apiKey);
-  var fullyConfiguredApiClient = sdk.RestApiClient(
-    apiDomain: apiDomain,
-    apiKey: apiKey,
-  );
-
-  var clientReflections = [
-    mirrors.reflect(anonApiClient),
-    // mirrors.reflect(altDomainApiClient),
-    // mirrors.reflect(authenticatedApiClient),
-    // mirrors.reflect(fullyConfiguredApiClient),
-  ];
-
-  var testFuncIMs = _getAllTestFuncInstanceMirrors();
-  var testSetsToRun = List<restApiClientTestSet>();
-
-  for (var tf in testFuncIMs) {
-    testSetsToRun.add(tf.reflectee());
-  }
-
-  for (var tset in testSetsToRun) {
-    testing.group(tset.testName, () {
-      var methodSym = mirrors.MirrorSystem.getSymbol(tset.methodName);
-
-      for (var tc in tset.cases) {
-        testing.test(tc.descr, () {
-          for (var clientRef in clientReflections) {
-            Map<Symbol, dynamic> namedArgs;
-
-            if (tc.namedArgs != null) {
-              tc.namedArgs.map((k, v) {
-                var kSym = mirrors.MirrorSystem.getSymbol(k);
-                return MapEntry(kSym, v);
-              });
-            }
-
-            clientRef.invoke(methodSym, tc.posArgs, namedArgs);
-          }
-        });
-      }
-    });
-  }
-}
-
-restApiClientTestSet f1() {
-  var testSet = restApiClientTestSet()
-    ..testName = "Assets"
-    ..methodName = "fetchAsset"
-    ..cases = [
-      restApiClientTestCase()..posArgs = ["btc"]
-    ];
-
-  return testSet;
-}
-
-restApiClientTestSet _f2() {
-  return restApiClientTestSet();
-}
-
-restApiClientTestSet _f3() {
-  return restApiClientTestSet();
-}
-
-List<mirrors.InstanceMirror> _getAllTestFuncInstanceMirrors() {
-  var testFuncIMs = List<mirrors.InstanceMirror>();
-
-  var ms = mirrors.currentMirrorSystem();
-  var retMir = mirrors.reflectClass(restApiClientTestSet);
-
-  // Reflect this library itself
-  var thisLib = ms.isolate.rootLibrary;
-
-  thisLib.declarations.forEach((k, decMir) {
-    if (decMir is! mirrors.MethodMirror) {
-      return;
-    }
-
-    mirrors.MethodMirror methodMir = decMir;
-    String methodName = mirrors.MirrorSystem.getName(k);
-
-    if (methodMir.isPrivate || methodMir.returnType != retMir) {
-      return;
-    }
-
-    var testFuncIM = thisLib.getField(decMir.simpleName);
-
-    testFuncIMs.add(testFuncIM);
-  });
-
-  return testFuncIMs;
+  restApiClientTestSuite.runTests();
 }
